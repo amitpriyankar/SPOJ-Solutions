65378316637b8fba6a83e5a267e941885e3c8e92
amitpriyankar <amitpriyankar22@gmail.com>
2019-06-06 11:12:36 +0530

    solved using segment trees

--- RMQSQ.cpp
+++ RMQSQ.cpp
@@ -2,7 +2,36 @@
 using namespace std;
 #define tani_nachi_ke  ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define M_PI 3.14159265358979323846
+int const N = 100009;
+int tree[4*N],arr[N];
 
+void build(int l, int r, int node)
+{
+  if(l == r)
+    {
+      tree[node] = arr[l];
+      return;
+    }
+
+  int mid = (l + r)/2;
+  build(l, mid, 2*node);
+  build(mid+1, r, 2*node + 1);
+  tree[node] = min(tree[2*node], tree[2*node+1]);
+}
+
+int querry(int l, int r, int node, int tl, int tr)
+{
+  if(l>tr || r<tl)
+    return INT_MAX;
+  else if(l>=tl && r<=tr)
+    return tree[node];
+  else
+  {
+    int mid = (l + r)/2;
+    return min(querry(l, mid, 2*node, tl ,tr), querry(mid+1, r, 2*node+1, tl, tr));
+  }
+
+}
 int main()
 {
 clock_t begin = clock();
@@ -13,45 +42,17 @@ freopen("output.txt", "w", stdout);
 tani_nachi_ke  
 int n;
 cin>>n;
-int arr[n];
-for(int i = 0; i < n; i++)
+for(int i = 1; i <= n; i++)
   cin>>arr[i];
-int len = (int)sqrt(n + 0.1) + 1;
-int dp[len];
-//initialising the array
-for(int i = 0; i < len; i++)
-  dp[i] = INT_MAX;
-
-//preprocessing
-for(int i = 0; i < n; i++)
-  dp[i/len] = min(dp[i/len], arr[i]);
+build(1, n, 1);
 
 int q,l,r;
 cin>>q;
-
-//queries solving
 while(q--)
 {
- 
   cin>>l>>r;
-  int c_l = l/len;
-  int c_r = r/len;
-  int ans = INT_MAX;
-  if( c_l == c_r)
-  {
-    for(int i = l; i <= r; i++)
-      ans = min(ans, arr[i]);
-  }
-  else
-  {
-    for(int i = l; i < (c_l + 1)*len; i++)
-      ans = min(ans, arr[i]);
-    for(int i = c_r*len; i <= r; i++)
-      ans = min(ans, arr[i]);
-    for(int i = c_l+1; i < c_r; i++)
-      ans = min(ans, dp[i]);
-  }
-  cout<<ans<<'\n';
+  l++; r++;
+  cout<<querry(1, n, 1, l, r)<<endl;
 }
 
 #ifndef ONLINE_JUDGE 
